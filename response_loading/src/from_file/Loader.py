c6b4ab1cc20a7278520efdd5bb53898682f150c6
Thomas Hayden <supremesteak1@gmail.com>
2016-10-20 13:42:15 -0400

    Improved commenting in Loader.py

--- response_loading/src/from_file/Loader.py
+++ response_loading/src/from_file/Loader.py
@@ -10,18 +10,17 @@ class Loader():
     def parse(self):
         # load = self.srcFile.readline()
         sheets = [ ]
-        previousSheet = Sheet("00000000")
+        previousSheet = Sheet("00000000") # Create some temp variables until proper checking can be put into place
         previousQuestion = Question("000")
-        for line in self.srcFile:
+        for line in self.srcFile: # Loop through all the lines in the file
             lst = list(line)
             if lst[0] == '#':
-                print "Comment found"
-            else:
-                print "No comment found; continuing parsing"
-                count = 0
+                print "Comment found" # Check if the line is a comment
+            else: # If not, continue working on it
+                count = 0 # Create a variable to store the indent level
                 for c in lst:
                     if c == ' ':
-                        count += 1
+                        count += 1 # Add to the indent level
                     else:
                         break
                 print "Found " + str(count) + " spaces"
@@ -38,14 +37,14 @@ class Loader():
                 elif count == 2:
                     # This is either a question type or question answer. Answers will have a '[' in front of them.
                     if lst[2] == '[':
-                        del lst[0:3]
+                        del lst[0:3] # Remove the leading spaces and '['
                         previousQuestion.setAnswer(''.join(lst))
                         print "Added answer " + ''.join(lst)
                     else:
-                        del lst[0:2]
+                        del lst[0:2] # Remove the leading spaces
                         previousQuestion.setType(''.join(lst))
                         print "Added type " + ''.join(lst)
-                    previousSheet.setQuestion(previousQuestion.getID,previousQuestion)
+                    previousSheet.setQuestion(previousQuestion.getID,previousQuestion) # Update the question with the new data
                     print "Updated question"
             print ""
 
